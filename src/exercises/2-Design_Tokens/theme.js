a068d15250096eca181ed7eae8c125e6d80b8586
Alex Kharouk <akharoukpp@gmail.com>
2019-04-10 11:34:35 +0200

    update colors

--- src/exercises/2-Design_Tokens/theme.js
+++ src/exercises/2-Design_Tokens/theme.js
@@ -1,8 +1,11 @@
 const theme = {
 	colors: {
-		'Awesome color to use when feeling blue': '#f39037',
-		'Best color to use when tired': '#f3a3a3',
-		'When dealing with CRO': '#cc0000',
+		primary: '#FF0084',
+		secondary: '#141f3f',
+		hover: '#FF9597',
+	},
+	typography: {
+		test: '#fff',
 	},
 };
 
