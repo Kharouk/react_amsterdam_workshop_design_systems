0cc687681cb956c8f446df44c8c61ce835e80960
Alex Kharouk <akharoukpp@gmail.com>
2019-04-10 12:28:32 +0200

    begin using styled components and create different states for button

--- src/exercises/3-Primitives/Button.js
+++ src/exercises/3-Primitives/Button.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SickButton = styled.button`
+	background: ${props => props.theme.colors.primary};
+	border-color: ${props => props.theme.colors.primary};
+	color: ${props => props.theme.colors.bg};
+	font-family: ${props => props.theme.fonts.base};
+	font-size: ${props => props.theme.fontSizes.base};
+	padding: 8px 16px;
+	border-radius: ${props => props.theme.radii.base};
+
+	&:hover {
+		background-color: ${props => props.theme.colors.hover};
+		border-color: ${props => props.theme.colors.hover};
+	}
+
+	&:active {
+		background: ${props => props.theme.colors.error};
+	}
+
+	&:focus {
+		transform: rotate(10800deg);
+		transition: 5s;
+	}
+`;
 
 /**
  * One button to push them all.
  */
 const Button = ({ children, ...props }) => {
-	return <button {...props}>{children}</button>;
+	return <SickButton {...props}>{children}</SickButton>;
 };
 
 Button.propTypes = {
